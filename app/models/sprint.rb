916301c4d461f931d95565a7e94a922a54f95161
thimmaiah <thimmaiah@gmail.com>
2014-03-08 23:12:27 -0500

    Better burndown chart

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -10,13 +10,17 @@ class Sprint < ActiveRecord::Base
     self.planned_hours  ||= 0
     self.actual_hours   ||= 0
   end
+  
+  def developers
+    self.project.project_user_mappings.team_members
+  end
 
   def days
     (end_date - start_date).to_i 
   end
 
   def man_days
-    days * self.project.project_user_mappings.team_members.count
+    days * developers.count
   end
   
   def available_hours
@@ -28,15 +32,16 @@ class Sprint < ActiveRecord::Base
   end
   
   def remaining_days
-
-    planned_days = (Date.today - start_date).to_i
+    
+    
+    planned_days = Date.today < end_date ? (Date.today - start_date).to_i : (end_date - start_date).to_i
 
     cum_hours = {}
     (0..planned_days).each do |day|
       cum_hours[day] ||= 0
     end
 
-    # get the cumulative hours logged per day
+    # get the cumulative hours logged per day till date
     self.tasks.each do |t|
       date = t.updated_at.to_date
       if date <= Date.today
@@ -46,15 +51,16 @@ class Sprint < ActiveRecord::Base
       end
     end
 
-    total_planned_hours = planned_days * Project::HOURS_PER_DAY
+    total_planned_hours = planned_days * developers.count * Project::HOURS_PER_DAY
 
+    cum_days = {}
     # Get the remaining hours after every days work
     (0..planned_days).each do |day|
       todays_hours = cum_hours[day]
-      cum_hours[day] = ((total_planned_hours - cum_hours[day])/Project::HOURS_PER_DAY).to_i
+      cum_days[day] = ((total_planned_hours - cum_hours[day])/(Project::HOURS_PER_DAY * developers.count)).to_i
       total_planned_hours -= todays_hours
     end
 
-    cum_hours
+    cum_days
   end
 end
